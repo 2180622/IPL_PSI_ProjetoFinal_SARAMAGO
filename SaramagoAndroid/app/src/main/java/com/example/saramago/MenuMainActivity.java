9ae4f346ff0b58b3e3948cd70d0a5867f1f03328
2180622 <2180622@my.ipleiria.pt>
2020-11-03 16:26:24 +0000

    Update de SaramagoAndroid

---
+++ SaramagoAndroid/app/src/main/java/com/example/saramago/MenuMainActivity.java
@@ -0,0 +1,21 @@
+package com.example.saramago;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MenuMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_main);
+    }
+}
+
+/*
+88BDBC - Azul Claro
+FFFFFF - Branco
+112D32 - Azul Super Escuro
+4F4A41 - Laranja Terra (Escuro)
+6E6658 - Cinzento Escuro
+*/
