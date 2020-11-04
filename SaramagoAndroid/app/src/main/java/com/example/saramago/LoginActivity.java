2ba70018c5e4716ce72f9135462b09be18ab94ca
2180622 <2180622@my.ipleiria.pt>
2020-11-04 17:18:54 +0000

    Update no SaramagoAndroidvarios
    primeiros layouts feitos e inicio da parte de login

--- SaramagoAndroid/app/src/main/java/com/example/saramago/LoginActivity.java
+++ SaramagoAndroid/app/src/main/java/com/example/saramago/LoginActivity.java
@@ -2,13 +2,48 @@ package com.example.saramago;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText etEmail, etPassword, etAPI;
+    private CheckBox cbAPI, cbGuardaSessao;
+    private String email;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        setTitle("Login");
+
+        etEmail = findViewById(R.id.etEmail);
+        etPassword = findViewById(R.id.etPassword);
+        etAPI = findViewById(R.id.etAPI);
+    }
+
+    public boolean isEmailValid(String email){
+        return Patterns.EMAIL_ADDRESS.matcher(email).matches();
+    }
+
+    public void onClickLogin(View view){
+        Context context = getApplicationContext();
+        email = etEmail.getText().toString();
+
+        if(!isEmailValid(email)){
+            Toast.makeText(context, "Email inválido", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        Intent intent = new Intent(this, MenuMainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
--- SaramagoAndroid/app/src/main/java/com/example/saramago/MenuMainActivity.java
+++ SaramagoAndroid/app/src/main/java/com/example/saramago/MenuMainActivity.java
@@ -1,14 +1,34 @@
 package com.example.saramago;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.FragmentManager;
+
 import android.os.Bundle;
 
+import com.google.android.material.navigation.NavigationView;
+
 public class MenuMainActivity extends AppCompatActivity {
+
+    public static final String NOME = "NOME";
+    private NavigationView navigationView;
+    private DrawerLayout drawer;
+    private String email = "";
+    private FragmentManager fragmentManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        navigationView = findViewById(R.id.nav_view);
+        drawer = findViewById(R.id.drawer_layout);
+        //ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer,
+              //  toolbar, "", "Fechar");
     }
 }
 
