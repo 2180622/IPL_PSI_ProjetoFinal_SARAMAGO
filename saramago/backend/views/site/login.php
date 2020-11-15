a6de31b57a03ce47b02d4371047425c73a78fbef
Gonçalo Rocha <goncalobcrocha@hotmail.com>
2020-11-15 12:48:45 +0000

    Mudanças no login /Backend // Update Composer

--- saramago/backend/views/site/login.php
+++ saramago/backend/views/site/login.php
@@ -10,21 +10,25 @@ use yii\bootstrap\ActiveForm;
 $this->title = 'Login';
 ?>
 <div class="site-login">
-    <h1><?= Html::encode($this->title) ?></h1>
 
-    <p>Please fill out the following fields to login:</p>
+    <h2 class="saramago-login">
+        <?=Html::img('@web/res/logo-saramago.png', ['style'=>'vertical-align:text-bottom','height'=>'50‰', 'alt'=>'SARAMAGO']) ?>
+        <?= Html::encode('.staff') ?>
+    </h2>
+    <h3><?= Html::encode($this->title)?></h3>
+    <p class="saramago-login">Preencha os seguintes campos para iniciar sessão:</p>
 
-            <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
+    <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
 
-                <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
+    <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
 
-                <?= $form->field($model, 'password')->passwordInput() ?>
+    <?= $form->field($model, 'password')->passwordInput() ?>
 
-                <?= $form->field($model, 'rememberMe')->checkbox() ?>
+    <?= $form->field($model, 'rememberMe')->checkbox() ?>
 
-                <div class="form-group">
-                    <?= Html::submitButton('Login', ['class' => 'btn btn-primary btn-block', 'name' => 'login-button']) ?>
-                </div>
+    <div class="form-group">
+        <?= Html::submitButton('Login', ['class' => 'btn-login btn-block', 'name' => 'login-button']) ?>
+    </div>
 
             <?php ActiveForm::end(); ?>
 </div>
