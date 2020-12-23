fbaad334ef668d317e6f865978856326d1d232e5
Gonçalo Rocha <goncalobcrocha@hotmail.com>
2020-12-23 12:12:48 +0000

    LoginTestCest (Criação) - Teste Funcional Obrigatorio
    O seguinte teste funcional não se encontra a funcionar de momento

---
+++ saramago/backend/tests/functional/LoginTestCest.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace backend\tests\functional;
+
+use common\fixtures\UserFixture;
+use backend\tests\FunctionalTester;
+
+class LoginTestCest
+{
+    /**
+     * @var \backend\tests\FunctionalTester
+     */
+
+    protected $tester;
+
+    /**
+     * Load fixtures before db transaction begin
+     * Called in _before()
+     * @see \Codeception\Module\Yii2::_before()
+     * @see \Codeception\Module\Yii2::loadFixtures()
+     * @return array
+     */
+    public function _fixtures()
+    {
+        /*return [
+            'user' => [
+                'class' => UserFixture::className(),
+                'dataFile' => codecept_data_dir() . 'login_data.php'
+            ]
+        ];*/
+    }
+
+    protected function formParams ($login, $password)
+    {
+        return [
+            'LoginForm[username]'=> $login,
+            'LoginForm[password]'=> $password,
+        ];
+    }
+
+    // tests
+    public function loginUser(FunctionalTester $I){
+        //$I->amOnRoute('site/login');
+        $I->amOnPage('site/login');
+        $I->seeElement('#login-form');
+        $I->submitForm('#login-form', $this->formParams('admin','adminsaramago'));
+        $I->click('login-button');
+
+        //ver o dropdown
+        $I->seeElement('#dropdown-toggle');
+        //ver a conta que está iniciada
+        $I->see('admin', '#dropdown-toggle');
+
+        //$I->click('login-button');
+        //$I->dontSeeLink('Login');
+        //$I->dontSeeLink('Signup');
+
+    }
+
+}
