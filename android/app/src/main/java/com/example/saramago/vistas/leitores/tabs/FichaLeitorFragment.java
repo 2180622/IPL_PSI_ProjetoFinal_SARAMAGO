d3b50e4601136b8be92eaab0be8ad8b4a616e995
2180622 <2180622@my.ipleiria.pt>
2021-01-04 21:54:26 +0000

    Ficha do Leitor Atualiza  ✔️

--- android/app/src/main/java/com/example/saramago/vistas/leitores/tabs/FichaLeitorFragment.java
+++ android/app/src/main/java/com/example/saramago/vistas/leitores/tabs/FichaLeitorFragment.java
@@ -10,9 +10,19 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.example.saramago.R;
+import com.example.saramago.modelos.Leitor;
+import com.example.saramago.modelos.SingletonGestorBiblioteca;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import org.w3c.dom.Text;
+
+import static com.example.saramago.vistas.leitores.DetalhesLeitorActivity.ID;
+
 public class FichaLeitorFragment extends Fragment {
+
+    private Leitor leitor;
+    private TextView nome, codBarras, nif, docId, dataNasc, morada, localidade, codPostal, telemovel, telefone, email, email2, dataRegisto, dataAtualizado, irregularidade;
+
     public FichaLeitorFragment() {
         // Required empty public constructor
     }
@@ -21,22 +31,48 @@ public class FichaLeitorFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_ficha_leitor, container, false);
 
-        TextView nome = view.findViewById(R.id.tv_fl_nome);
-        TextView codBarras = view.findViewById(R.id.tv_fl_codBarras);
-        TextView nif = view.findViewById(R.id.tv_fl_nif);
-        TextView docId = view.findViewById(R.id.tv_fl_docId);
-        TextView dataNasc = view.findViewById(R.id.tv_fl_dataNasc);
-        TextView morada = view.findViewById(R.id.tv_fl_morada);
-        TextView localidade = view.findViewById(R.id.tv_fl_localidade);
-        TextView codPostal = view.findViewById(R.id.tv_fl_codPostal);
-        TextView telemovel = view.findViewById(R.id.tv_fl_telemovel);
-        TextView telefone = view.findViewById(R.id.tv_fl_telefone);
-        TextView email = view.findViewById(R.id.tv_fl_mail);
-        TextView email2 = view.findViewById(R.id.tv_fl_mail2);
-        TextView dataRegisto = view.findViewById(R.id.tv_fl_dtaRegisto);
-        TextView dataAtualizado = view.findViewById(R.id.tv_fl_dtaAtualizado);
-        TextView irregularidade = view.findViewById(R.id.tv_fl_irregularidade);
+        int id = getActivity().getIntent().getIntExtra(ID, -1);
+        leitor = SingletonGestorBiblioteca.getInstance(getContext()).getLeitor(id);
+
+        nome = view.findViewById(R.id.tv_fl_nome);
+        codBarras = view.findViewById(R.id.tv_fl_codBarras);
+        nif = view.findViewById(R.id.tv_fl_nif);
+        docId = view.findViewById(R.id.tv_fl_docId);
+        dataNasc = view.findViewById(R.id.tv_fl_dataNasc);
+        morada = view.findViewById(R.id.tv_fl_morada);
+        localidade = view.findViewById(R.id.tv_fl_localidade);
+        codPostal = view.findViewById(R.id.tv_fl_codPostal);
+        telemovel = view.findViewById(R.id.tv_fl_telemovel);
+        telefone = view.findViewById(R.id.tv_fl_telefone);
+        email = view.findViewById(R.id.tv_fl_mail);
+        email2 = view.findViewById(R.id.tv_fl_mail2);
+        dataRegisto = view.findViewById(R.id.tv_fl_dtaRegisto);
+        dataAtualizado = view.findViewById(R.id.tv_fl_dtaAtualizado);
+        irregularidade = view.findViewById(R.id.tv_fl_irregularidade);
+
+
+        if(leitor != null){
+            carregarConteudo();
+        }
 
         return view;
+    }
+
+    private void carregarConteudo(){
+        nome.setText(leitor.getNome());
+        codBarras.setText(leitor.getCodBarras());
+        nif.setText(leitor.getNif()+"");
+        docId.setText(leitor.getDocId());
+        dataNasc.setText(leitor.getDataNasc());
+        morada.setText(leitor.getMorada());
+        localidade.setText(leitor.getLocalidade());
+        codPostal.setText(leitor.getCodPostal()+"");
+        telemovel.setText(leitor.getTelemovel()+"");
+        telefone.setText(leitor.getTelefone()+"");
+        email.setText(leitor.getEmail());
+        email2.setText(leitor.getMail2());
+        dataRegisto.setText(leitor.getDataRegisto()+"");
+        dataAtualizado.setText(leitor.getDataAtualizado()+"");
+        irregularidade.setText("Sem Irregularidades");
     }
 }
