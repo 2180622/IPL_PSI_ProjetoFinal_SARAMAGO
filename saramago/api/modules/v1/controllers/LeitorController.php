7dec13f2140a41e4f2d8b2cad20b7c06a738ea83
Gonçalo Rocha <goncalobcrocha@hotmail.com>
2021-01-13 22:58:34 +0000

    Update LeitorController.php

--- saramago/api/modules/v1/controllers/LeitorController.php
+++ saramago/api/modules/v1/controllers/LeitorController.php
@@ -1,55 +1,17 @@
 <?php
+
 namespace api\modules\v1\controllers;
 
-use Yii;
 use yii\rest\ActiveController;
-use yii\web\Controller;
-use yii\filters\VerbFilter;
-use yii\filters\AccessControl;
 
-/**
- * Leitor controller
- */
 class LeitorController extends ActiveController
 {
-    /**
-     * {@inheritdoc}
-     */
-    public function behaviors()
+
+    public $modelClass = 'common\models\Leitor';
+
+    public function actionIndex()
     {
-        return [
-            'access' => [
-                'class' => AccessControl::className(),
-                'rules' => [
-                    [
-                        'actions' => ['login', 'error'],
-                        'allow' => true,
-                    ],
-                    [
-                        'actions' => ['logout', 'index'],
-                        'allow' => true,
-                        'roles' => ['@'],
-                    ],
-                ],
-            ],
-            'verbs' => [
-                'class' => VerbFilter::className(),
-                'actions' => [
-                    'logout' => ['post'],
-                ],
-            ],
-        ];
+        return $this->render('site/index');
     }
 
-    /**
-     * {@inheritdoc}
-     */
-    public function actions()
-    {
-        return [
-            'error' => [
-                'class' => 'yii\web\ErrorAction',
-            ],
-        ];
-    }
 }
