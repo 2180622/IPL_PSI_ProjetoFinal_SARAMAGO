066c7ee54486c5dd5cf5bc1ea3934d8873c0e5bc
Gonçalo Rocha <goncalobcrocha@hotmail.com>
2021-01-16 01:29:32 +0000

    LoginController - Feito

--- saramago/api/modules/v1/controllers/LoginController.php
+++ saramago/api/modules/v1/controllers/LoginController.php
@@ -5,46 +5,67 @@ namespace api\modules\v1\controllers;
 use common\models\User;
 use Yii;
 use yii\filters\auth\HttpBasicAuth;
+use yii\filters\auth\QueryParamAuth;
 use yii\filters\ContentNegotiator;
 use yii\rest\ActiveController;
 use yii\rest\Controller;
 use yii\web\HttpException;
 use yii\web\Response;
-use yii\web\IdentityInterface;
 
-class LoginController extends ActiveController
+class LoginController extends Controller
 {
-    public $modelClass = 'common\models\User';
 
     public function behaviors()
     {
-        $behaviors = parent::behaviors();
-
-        $behaviors['authenticator'] = [
-            'class' => HttpBasicAuth::className(),
-            'auth' => [$this, 'auth']
-        ];
-
         $behaviors['contentNegotiator'] = [
             'class' => ContentNegotiator::className(),
             'formats' => [
-                'application/json' => Response::FORMAT_XML,
+                'application/json' => Response::FORMAT_JSON,
             ],
         ];
-
         return $behaviors;
     }
 
-    public function auth($username,$password)
+    //Actions List
+    public function actionIndex()
     {
-        $user = User::findOne(['username' => $username]);
-
-        if($user!=null && $user->validatePassword($password))
-        {
-            $auth_key = $user->auth_key;
-
-            return $auth_key;
-        }
-        throw new HttpException('401', 'O username ou a password está incorreta.');
+        $response = [
+            'POST /login' => ['username','password'],
+        ];
+        return $response;
     }
+
+    public function actionLogin()
+    {
+        //SE ELE FIZER UM POST
+        if($post=\Yii::$app->request->post())
+        {
+            if ((isset($post["username"]) && isset($post["password"])) != null)
+            {
+                $user = User::findByUsername($post["username"]);
+                if($user && $user->validatePassword($post["password"]))
+                {
+                    return [
+                        "id"=>$user->id,
+                        "username"=>$user->username,
+                        "token"=>$user->getAuthKey(),
+                    ];
+                }
+            }
+            else
+                {
+                    throw new HttpException('406', 'O username ou a password está em falta');
+                }
+
+        throw new HttpException('401', 'O username ou a password está incorreta');
+        }
+
+        //SE FIZER POST, SEM FORM
+        $response = [
+            'username' => 'username',
+            'password' => 'password',
+        ];
+        return $response;
+    }
+
 }
